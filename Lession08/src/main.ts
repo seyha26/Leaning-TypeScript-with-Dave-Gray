79b4932831e9a5358be267ccbcb27a5bbe09e865
seyha26 <seyhachheng26@email.com>
2023-10-27 10:24:55 +0700

    finish lession 08

---
+++ Lession08/src/main.ts
@@ -0,0 +1,153 @@
+const echo = <T>(arg: T): T => arg;
+const isObj = <T>(arg: T): boolean => {
+  return typeof arg === "object" && !Array.isArray(arg) && arg !== null;
+};
+
+console.log(isObj("Seyha"));
+console.log(isObj(true));
+console.log(isObj(null));
+console.log(isObj({ name: "jonh" }));
+console.log(isObj(["null", "test"]));
+
+const isTrue = <T>(arg: T): { arg: T; is: boolean } => {
+  if (Array.isArray(arg) && !arg.length) {
+    return { arg, is: false };
+  }
+  if (isObj(arg) && !Object.keys(arg as keyof T).length) {
+    return { arg, is: false };
+  }
+  return { arg, is: !!arg };
+};
+
+console.log(isTrue(0));
+console.log(isTrue(true));
+console.log(isTrue(1));
+console.log(isTrue("Dave"));
+console.log(isTrue(""));
+console.log(isTrue(null));
+console.log(isTrue(undefined));
+console.log(isTrue({}));
+console.log(isTrue({ name: "Jonh" }));
+console.log(isTrue([]));
+console.log(isTrue([1, 2, 3]));
+console.log(isTrue(NaN));
+console.log(isTrue(-0));
+
+interface BoolCheck<T> {
+  value: T;
+  is: boolean;
+}
+
+const checkBooleanValue = <T>(arg: T): BoolCheck<T> => {
+  if (Array.isArray(arg) && !arg.length) {
+    return { value: arg, is: false };
+  }
+  if (isObj(arg) && !Object.keys(isObj).length) {
+    return { value: arg, is: false };
+  }
+  return { value: arg, is: !!arg };
+};
+
+// console.log(checkBooleanValue(0));
+// console.log(checkBooleanValue(true));
+// console.log(checkBooleanValue(1));
+// console.log(checkBooleanValue("Dave"));
+// console.log(checkBooleanValue(""));
+// console.log(checkBooleanValue(null));
+// console.log(checkBooleanValue(undefined));
+// console.log(checkBooleanValue({}));
+// console.log(checkBooleanValue({ name: "Jonh" }));
+// console.log(checkBooleanValue([]));
+// console.log(checkBooleanValue([1, 2, 3]));
+// console.log(isTrue(NaN));
+// console.log(checkBooleanValue(-0));
+
+interface HasId {
+  id: number;
+}
+
+const processUser = <T extends HasId>(user: T): T => {
+  return user;
+};
+console.log(processUser({ id: 1, name: "seyha" }));
+// console.log(processUser({ name: "seyha" }));
+
+const getUsersProperty = <T extends HasId, K extends keyof T>(
+  users: T[],
+  key: K
+): T[K][] => {
+  return users.map((user) => user[key]);
+};
+
+const usersArray = [
+  {
+    id: 1,
+    name: "Leanne Graham",
+    username: "Bret",
+    email: "Sincere@april.biz",
+    address: {
+      street: "Kulas Light",
+      suite: "Apt. 556",
+      city: "Gwenborough",
+      zipcode: "92998-3874",
+      geo: {
+        lat: "-37.3159",
+        lng: "81.1496",
+      },
+    },
+    phone: "1-770-736-8031 x56442",
+    website: "hildegard.org",
+    company: {
+      name: "Romaguera-Crona",
+      catchPhrase: "Multi-layered client-server neural-net",
+      bs: "harness real-time e-markets",
+    },
+  },
+  {
+    id: 2,
+    name: "Ervin Howell",
+    username: "Antonette",
+    email: "Shanna@melissa.tv",
+    address: {
+      street: "Victor Plains",
+      suite: "Suite 879",
+      city: "Wisokyburgh",
+      zipcode: "90566-7771",
+      geo: {
+        lat: "-43.9509",
+        lng: "-34.4618",
+      },
+    },
+    phone: "010-692-6593 x09125",
+    website: "anastasia.net",
+    company: {
+      name: "Deckow-Crist",
+      catchPhrase: "Proactive didactic contingency",
+      bs: "synergize scalable supply-chains",
+    },
+  },
+];
+
+console.log(getUsersProperty(usersArray, "email"));
+
+class StateObject<T> {
+  private data: T;
+  constructor(value: T) {
+    this.data = value;
+  }
+
+  get state(): T {
+    return this.data;
+  }
+  set state(value: T) {
+    this.data = value;
+  }
+}
+
+const store = new StateObject("Jonh");
+console.log(store.state);
+// store.state =12;
+
+const myState = new StateObject<(string | number | boolean)[]>(["Seyha"]);
+myState.state = ["Dev", 26, true];
+console.log(myState.state);
